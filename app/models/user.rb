1f1c8d9e4dda49be854b29f34a8b8150f16c8d60
menna-ga <menna-gamal@hotmail.com>
2017-06-19 19:16:51 -0400

    added validation for the name & email address (step 2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_one :address
   accepts_nested_attributes_for :address
+
+  validates :name, :email_address, :presence => true
+  validates :name, :length => { :minimum => 2 }
+  validates :email_address, :uniqueness => true
 end
